cb197f0337b8ce75714cb952f8a4e8c80e36d28d
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2025-05-04 13:41:25 +0100

    port removed for production

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import app from './app.js';
 
-import { PORT } from './config/env.js';
+import { NODE_ENV, PORT } from './config/env.js';
 import connectToDB from './database/mongodb.js';
 
 const server = app.listen(PORT, async () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
+  if (NODE_ENV === 'development') {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
+  }
 
   await connectToDB();
 });
