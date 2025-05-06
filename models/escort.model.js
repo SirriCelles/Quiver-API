c253f6915a99d49fa645ed2da4e0258531fc9837
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2025-05-07 00:22:08 +0100

    add escort model

--- models/escort.model.js
+++ models/escort.model.js
@@ -1,17 +1,69 @@
 import mongoose from 'mongoose';
 
-const escortModel = mongoose.Schema({
-  role: {
-    type: String,
-    default: 'escort',
+const escortSchema = mongoose.Schema({
+  //  inherits all User fields via application logic
+  userRef: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [
+      true,
+      'Please provide you full user details to become an escort',
+    ],
   },
-  phoneNumber: {
-    type: Number,
-  },
-  location: {
-    type: Location,
-  },
-  dateOfBirth: {
-    type: Date,
+  services: [
+    {
+      name: {
+        type: String,
+        required: true,
+        enum: ['tour_guide', 'dining', 'adventure'],
+      },
+      hourlyRate: {
+        type: Number,
+        required: true,
+      },
+      description: String,
+    },
+  ],
+  availability: [
+    {
+      day: {
+        type: String,
+        enum: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+      },
+      slots: [
+        {
+          start: {
+            type: Date,
+            required: [true, 'Start date must be provided for avialable slots'],
+          },
+          end: {
+            type: Date,
+            required: [true, 'End date must be provided for avialable slots'],
+          },
+        },
+      ],
+    },
+  ],
+  tags: [
+    {
+      type: String,
+      index: true,
+    },
+  ],
+  stats: {
+    completedBookings: {
+      type: Number,
+      default: 0,
+    },
+    averageRating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    responseTime: Number, //Average in minutes
   },
 });
+
+const Escort = mongoose.model('Escort', escortSchema);
+
+export default Escort;
